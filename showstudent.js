6a72e00c68f39da949b2c0a6e6bd774e4e7fa3f8
willy031110 <411630063@o365.tku.edu.tw>
2024-11-20 20:22:49 +0800

    1120up

--- showstudent.js
+++ showstudent.js
@@ -1,28 +1,4 @@
-/*const fs = require('fs');
-const csv = require('csv-parser');
-const {MongoClient}= require('mongodb');
-const url ="mongodb://localhost:27017";
-const dbName= "411630063";
-const collectionName="studentlist";
-(async ()=>{
-    const client= new MongoClient(url);
-try {
-    // 連接到 MongoDB
-    await client.connect();
-    console.log("成功連接到 MongoDB");
 
-    const db = client.db(dbName);
-    const studentslist = await collection.find().toArray();
-
-    console.log("學生資料列表:");
-    studentslist.forEach(studentlist => {
-        console.log(studentlist)
-        
-    });
-} catch (error) {
-    console.error("發生錯誤：", error);
-}
-})();*/
 const fs = require('fs');
 const csv = require('csv-parser');
 const { MongoClient } = require('mongodb');
